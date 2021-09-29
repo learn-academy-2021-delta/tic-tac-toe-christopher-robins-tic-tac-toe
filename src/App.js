10628569543462c3dee31ddce9a1d103fda3336f
alappie <ashley@specialtyproduce.com>
2021-09-29 10:55:01 -0700

    In to the Woods

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      xmark: null,
+      omark: null
     }
+  }
+  componentDidMount(){
+    let X = "X"
+    let O = "O"
+    this.setState({xmark: X, omark: O})
+  }
+  handleGamePlay = (index) => {
+    const { squares, xmark, omark }= this.state
+
   }
 
   render(){
+    console.log("handleGamePlay", this.state.handleGamePlay)
     return(
       <>
         <h1>Tic Tac Toe</h1>
@@ -18,7 +30,11 @@ class App extends Component{
           {this.state.squares.map((value, index)=>{
           return( <Square 
           value={value}
-          index={index}/>) 
+          index={index}
+          key={index}
+          handleGamePlay={this.handleGamePlay}
+          />
+          ) 
           })}
         </div>
         
